db4e8c65c48737437c8358969a3d112eeef3117a
Aayam Bhattarai <aayamgt@hotmail.com>
2023-06-24 03:06:42 -0400

    Completed Simple Calculator

--- app.js
+++ app.js
@@ -5,18 +5,19 @@ const clearBtn = document.getElementById("data-clear");
 
 let fArg = 0;
 let operator = '';
+let foperator = '';
+let loperator = '';
 let lArg = 0;
 let counter = 0;
+let oCounter = 0;
 
 numBtn.forEach( (button) =>
     button.addEventListener('click', () => {
     display(button.textContent);
     if (counter === 0){
         fArg = dScreen.textContent;
-        console.log(`fArg ${fArg}`);
     } else {
         lArg = dScreen.textContent;
-        console.log(`lArg ${lArg}`);
     };
 })
 );
@@ -24,18 +25,51 @@ numBtn.forEach( (button) =>
 opBtn.forEach( (button) =>
     button.addEventListener('click', () => {
     operator = button.textContent;
+    if (oCounter === 0){
+        foperator = button.textContent;
+        oCounter += 1;
+    } else if (oCounter != 0 && operator != '='){
+        foperator = operator;
+    }
     counter += 1;
-    clear();
+    clearScreen();
+    if (operator === '='){
+        counter = 0;
+        dScreen.textContent = evaluate(fArg, lArg, foperator);
+    }   
 })
 );
 
-clearBtn.addEventListener('click', clear);
+clearBtn.addEventListener('click', clearScreen, clearAll);
 
 function display(number){
     dScreen.textContent += number;        
 }
 
-function clear(){
+function clearScreen(){
     dScreen.textContent = '';
-    fArg = 0;
 }
+
+function clearAll(){
+    counter = 0;
+    oCounter = 0;
+    fArg = 0 ;
+    lArg = 0;
+    foperator = 0;
+    loperator = 0;
+}
+
+function evaluate(num1, num2, operator){
+    if (operator === '+'){
+        return parseInt(num1) + parseInt(num2);
+    } else if (operator === '-'){
+        return parseInt (num1) - parseInt(num2);
+    } else if (operator === '*'){
+        return parseInt (num1) * parseInt(num2);
+    } else if (operator === '/'){
+        return parseInt (num1) / parseInt(num2);
+    } else {
+        return "NAMASTE";
+    }
+}
+
